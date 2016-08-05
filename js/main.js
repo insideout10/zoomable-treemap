cf2b08babe746e5597586640bef0cc2664f598a9
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2016-08-05 15:41:43 +0200

    add handlebars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-require('./d3.min');
-require('./jquery.min');
+jQuery = $ = require('./jquery.min');
+d3 = require('./d3.min');
+Handlebars = require('handlebars');
 require('./treemap');
+
+var source = $('#tile_template').html();
+var tpl = Handlebars.compile(source);
+var obj = {
+  titolo: 'ciao titolo',
+  descrittore: 'bla blas vlaaaaaaaaaa aaaaa aaa aa aa a .'
+};
+
+$('body').append( tpl(obj) );
