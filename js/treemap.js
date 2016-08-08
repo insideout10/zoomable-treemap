4e4e3aa24566fdf181121abb09c1001843baa7dd
pieroit <piero.savastano@gmail.com>
2016-08-08 13:56:46 +0200

    add basic navigation

--- js/treemap.js
+++ js/treemap.js
@@ -48,31 +48,52 @@ Treemap.prototype.initTreemap = function(){
     treemapObj.config.height = parseInt( treemapObj.selection.style('height'), 10 );
     
     // Add breadcumbs / navigation bar
-    treemapObj.breadcumbsHeight = 50;
-    treemapObj.selection.append('div')
-        .attr('id', 'treemap-breadcumbs')
+    treemapObj.navigationHeight = 50;
+    var navigation = treemapObj.selection.append('div')
+        .attr('id', 'treemap-navigation')
         .style('width', '100%')
-        .style('height', treemapObj.breadcumbsHeight + 'px');
+        .style('height', treemapObj.navigationHeight + 'px');
+
+    // Add breadcumbs text and buttons
+    navigation.append('span')
+        .attr('id', 'treemap-navigation-breadcumbs');
+    navigation.append('span')
+        .attr('id', 'treemap-navigation-up')
+        .text(' ___up')
+        .on('click', function(){
+            treemapObj.upOneLevel();
+        });
+    navigation.append('span').text('home')
+        .attr('id', 'treemap-navigation-home') 
+        .text(' ___home')
+        .on('click', function(){
+            treemapObj.upToTopLevel();
+        });
 
     // Add tiles container
-    treemapObj.tilesContainerHeight = treemapObj.config.height - treemapObj.breadcumbsHeight;
+    treemapObj.tilesContainerHeight = treemapObj.config.height - treemapObj.navigationHeight;
     treemapObj.selection.append('div')
         .attr('id', 'treemap-tiles-container')
         .style('width', '100%')
         .style('height', treemapObj.tilesContainerHeight + 'px')
         .style('position', 'relative');
 };
-    
+
+
 Treemap.prototype.updateTreemap = function(node){
     
     // Reference to current obj
     var treemapObj = this;
+    
+    // Store the node being displayed
+    treemapObj.currentNode = node;
         
     var layoutSize = [treemapObj.config.width, treemapObj.tilesContainerHeight];
     treemapObj.layout = d3.treemap()
         .size( layoutSize );
 
-    layoutNode = treemapObj.layout(node);
+    var nodeCopy = node.copy();
+    layoutNode = treemapObj.layout(nodeCopy);
     console.log(layoutNode);
 
     treemapObj.selection.selectAll('.node').remove();
@@ -83,7 +104,6 @@ Treemap.prototype.updateTreemap = function(node){
         .attr('class', 'node')
         .style('position', 'absolute')
         .style('top', function(d){
-            console.log(d.y0, d.x0, d.x1 - d.x0, d.y1 - d.y0);
             return d.y0 + 'px';
         })
         .style('left', function(d){
@@ -96,7 +116,6 @@ Treemap.prototype.updateTreemap = function(node){
             return (d.y1 - d.y0) + 'px';
         })
         .style('background-color', function(d, i){
-            console.log(d3);
             return d3.schemeCategory20[i%20];
         })
         .html(function(d){
@@ -110,7 +129,19 @@ Treemap.prototype.updateTreemap = function(node){
             }
         });
 };
+
+Treemap.prototype.upOneLevel = function(){
     
+    // display parent node
+    this.updateTreemap(this.currentNode.parent);
+};
+
+Treemap.prototype.upToTopLevel = function(){
+    
+    // display root node
+    this.updateTreemap(this.data);
+};
+
 module.exports = Treemap;
 
 
