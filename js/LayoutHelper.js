718fae31c5e280b6bcacfe95506fb605f149fba4
pieroit <piero.savastano@gmail.com>
2016-08-10 19:54:05 +0200

    aggregate tiles if they are too small

--- js/LayoutHelper.js
+++ js/LayoutHelper.js
@@ -10,8 +10,8 @@ var LayoutHelper = function(layoutSize, config){
     this.layout = d3.treemap()
         .size( layoutSize )
         //.tile(d3.treemapSlice)
-        .tile(d3.treemapBinary)
-        //.tile(d3.treemapSquarify)
+        //.tile(d3.treemapBinary)
+        .tile(d3.treemapSquarify)
         .padding(5);
         // TODO: configuration
 };
@@ -23,60 +23,115 @@ LayoutHelper.prototype.getAdjustedLayout = function(node){
     if(!this.config.minWidth || !this.config.minHeight){
         return this.layout(node.copy());   // copy node and descendants, so the laytout does not mess with original data (no side effects)
     }
-    
-    var shrinkedNode, shrinkedNodeLayout;
+
     var numberOfTilesToBeAggregated = 2;
+    var shrinkedNode = node.copy();
+    var shrinkedNodeLayout;
+    var tinyTiles;
     do {
-        shrinkedNode       = node.copy();   // copy node and descendants, so the laytout does not mess with original data (no side effects)
-        shrinkedNodeLayout = this.layout(shrinkedNode);
+        
+        // Compute layout
+        shrinkedNodeLayout = this.layout(shrinkedNode.copy());   // copy node and descendants, so the laytout does not mess with original data (no side effects)
+        
+        shrinkedNodeLayout.children.forEach(function(c){
+            console.log(c.data.name, c.x1, c);
+        });
+        console.log('\n');
         
         // Check if there are tiles too small
         var tinyTiles = this.getTilesNotRespectingMinimalSize(shrinkedNodeLayout);
         
         if(tinyTiles.length > 0){
             // Adjust tiles
-            shrinkedNode = this.aggregateNSmallestNodes(node, tinyTiles, numberOfTilesToBeAggregated);
+            var shrinkedNode = this.aggregateNSmallestTiles(node.copy(), tinyTiles, numberOfTilesToBeAggregated);
+            shrinkedNodeLayout = this.layout(shrinkedNode.copy());
             numberOfTilesToBeAggregated++;
         } else {
             return shrinkedNodeLayout;
         }
         
+        ////////////////
+        if(numberOfTilesToBeAggregated == 4)
+            tinyTiles = []
+        ////////////////
     } while( tinyTiles.length > 0 && numberOfTilesToBeAggregated < node.children.length );
 
     return shrinkedNodeLayout;
     
 };
 
-// Check if all tiles respect the minimal desired size
+// Get tiles not respecting the minimal desired size
 LayoutHelper.prototype.getTilesNotRespectingMinimalSize = function(node){
+    
+    /*node.children.forEach(function(c){
+        console.log(c.data.name);
+    });
+    console.log('\n');*/
     
     var minimalWidth  = this.config.minWidth;
     var minimalHeight = this.config.minHeight;
     var nodesWhichAreTooSmall = [];
     
-    for(var i=0; i<node.children.length; i++){
-        var tile = node.children[i];
+    node.children.forEach(function(tile){
         var tileWidth  = tile.x1 - tile.x0;
         var tileHeight = tile.y1 - tile.y0;
         if( tileWidth < minimalWidth || tileHeight < minimalHeight ){
             nodesWhichAreTooSmall.push(tile);
         }
-    }
+    });
     return nodesWhichAreTooSmall;
 }
 
 // Fuse the two smallest tiles which are smaller than the required size
-LayoutHelper.prototype.aggregateNSmallestNodes = function(node, tinyTiles, numberOfTilesToBeAggregated){
+LayoutHelper.prototype.aggregateNSmallestTiles = function(node, tinyTiles, numberOfTilesToBeAggregated){
+    
+    
+    /*node.children.forEach(function(c){
+        console.log(c.data.name);
+    });*/
+    
+    
+    // keep a reference to current obj
+    var layoutHelper = this;
 
-    console.log('AGGREGATING', node, tinyTiles, numberOfTilesToBeAggregated);
+    // sort tinyTiles by area
+    tinyTiles.sort(function(a, b){
+        //return layoutHelper.tileArea(a) - layoutHelper.tileArea(b);
+        return a.value - b.value;
+    });
+    var tilesToBeAggregated = tinyTiles.slice(0, numberOfTilesToBeAggregated);
+    //console.log(tinyTiles, tilesToBeAggregated, '\n'); 
+    
+    var newTile = {
+        data     : {
+            name     : 'ALTRO'
+        },
+        parent   : node,
+        children : [],
+        value    : 0,
+    };
+    //console.log('AGGREGATIN', node, tilesToBeAggregated, numberOfTilesToBeAggregated);
+    var indexesOfTilesToBeAggregated = [];
+    tilesToBeAggregated.forEach(function(t){
+        node.children.forEach(function(c, i){
+            if(c.data == t.data){
+                newTile.children.push(c);
+                newTile.value += c.value;
+                indexesOfTilesToBeAggregated.push(i);
+            }
+        });
+    });
+    
+    node.children = node.children.filter(function(c, i){
+        return indexesOfTilesToBeAggregated.indexOf(i) == -1;
+    });
+    
+    node.children.push(newTile);
     return node;
 };
 
-// Aggregate the two smallest tiles to respect minimal tile size
-LayoutHelper.prototype.aggregateTwoSmallestTiles = function(){
-    
-    
-    
+LayoutHelper.prototype.tileArea = function(tile){
+    return (tile.x1 - tile.x0)*(tile.y1 - tile.y0);
 }
 
 module.exports = LayoutHelper;
