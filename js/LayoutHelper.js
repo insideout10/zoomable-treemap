0cc22b3f3a7ae7162a7b9c830152515ae9a1574c
David Riccitelli <david@insideout.io>
2018-03-01 18:21:00 +0100

    update files

--- js/LayoutHelper.js
+++ js/LayoutHelper.js
@@ -2,177 +2,186 @@
 
 var d3 = require('./../node_modules/d3/build/d3.min.js');
 
-var LayoutHelper = function(layoutSize, isMobilePortrait, config){
+var LayoutHelper = function(layoutSize, isMobilePortrait, config) {
+  this.config = config.tiles;
+  this.config.tileAggregationCallback = config.tileAggregationCallback;
 
-    this.config = config.tiles;
-    this.config.tileAggregationCallback = config.tileAggregationCallback;
-    
-    // Padding
-    this.config.padding = this.config.padding || 0;
-    
-    // Flattening factor. Regulates how much tiles are forced to be of the same dimensions
-    this.config.flatteningFactor = config.flatteningFactor || 10;
-    
-    // Store if we are in vertical/mobile layout or not
-    this.isMobilePortrait = isMobilePortrait;
-    
-    // Use different tiling methods for mobile portrait vs. all the rest
-    var tilingAlgorithm;
-    if(isMobilePortrait){
-        // just cut treemap vertically
-        tilingAlgorithm = d3.treemapSlice;
-    } else {
-        // Tile ratio
-        var ratio = 1.618034;   // golden ratio by default
-        if(config.minHeight && config.minWidth){
-            ratio = config.minWidth / config.minHeight; // overwrite if necessary
-        }
-        tilingAlgorithm = d3.treemapSquarify.ratio(ratio);
+  // Padding
+  this.config.padding = this.config.padding || 0;
+
+  // Flattening factor. Regulates how much tiles are forced to be of the same dimensions
+  this.config.flatteningFactor = config.flatteningFactor || 10;
+
+  // Store if we are in vertical/mobile layout or not
+  this.isMobilePortrait = isMobilePortrait;
+
+  // Use different tiling methods for mobile portrait vs. all the rest
+  var tilingAlgorithm;
+  if (isMobilePortrait) {
+    // just cut treemap vertically
+    tilingAlgorithm = d3.treemapSlice;
+  } else {
+    // Tile ratio
+    var ratio = 1.618034; // golden ratio by default
+    if (config.minHeight && config.minWidth) {
+      ratio = config.minWidth / config.minHeight; // overwrite if necessary
     }
-    
-    // Prepare layout generator
-    this.layout = d3.treemap()
-        .size( layoutSize )
-        .tile(tilingAlgorithm)
-        .padding(this.config.padding);
+    tilingAlgorithm = d3.treemapSquarify.ratio(ratio);
+  }
+
+  // Prepare layout generator
+  this.layout = d3
+    .treemap()
+    .size(layoutSize)
+    .tile(tilingAlgorithm)
+    .padding(this.config.padding);
 };
 
 // Adjust layout if a minimal tile size is required
-LayoutHelper.prototype.getAdjustedLayout = function(node){
-    
-    // No adjustments required
-    if(!this.config.minWidth || !this.config.minHeight || !this.config.minHeightMobile){
-        return this.layout(node.copy());   // copy node and descendants, so the laytout does not mess with original data (no side effects)
+LayoutHelper.prototype.getAdjustedLayout = function(node) {
+  // No adjustments required
+  if (
+    !this.config.minWidth ||
+    !this.config.minHeight ||
+    !this.config.minHeightMobile
+  ) {
+    return this.layout(node.copy()); // copy node and descendants, so the laytout does not mess with original data (no side effects)
+  }
+
+  var tilesSortedBySize = node.children.slice();
+  tilesSortedBySize.sort(function(a, b) {
+    return a.value - b.value;
+  });
+
+  var numberOfTilesToBeAggregated = 1;
+  do {
+    // Aggregate n smallest tiles
+    var shrinkedNode = this.aggregateNSmallestTiles(
+      node.copy(),
+      tilesSortedBySize,
+      numberOfTilesToBeAggregated
+    );
+
+    // Flatten tiles (big tiles get smaller and small tiles get bigger)
+    shrinkedNode = this.flattenTilesValues(shrinkedNode);
+
+    // Recompute layout (tiles coordinates and size on screen)
+    var shrinkedNodeLayout = this.layout(shrinkedNode.copy());
+
+    console.log(node);
+    // Is minimal tile size respected?
+    if (this.everyTileIsRespectingMinimalSize(shrinkedNodeLayout)) {
+      // ... yes, return layout
+      return shrinkedNodeLayout;
+    } else {
+      // ... no, we must aggregate more tiles
+      numberOfTilesToBeAggregated++;
     }
+  } while (numberOfTilesToBeAggregated < node.children.length);
 
-    var tilesSortedBySize = node.children.slice();
-    tilesSortedBySize.sort(function(a, b){
-        return a.value - b.value;
-    });
-    
-    var numberOfTilesToBeAggregated = 1;
-    do {
-        
-        // Aggregate n smallest tiles
-        var shrinkedNode = this.aggregateNSmallestTiles(node.copy(), tilesSortedBySize, numberOfTilesToBeAggregated);
-        
-        // Flatten tiles (big tiles get smaller and small tiles get bigger)
-        shrinkedNode = this.flattenTilesValues(shrinkedNode);
-        
-        // Recompute layout (tiles coordinates and size on screen)
-        var shrinkedNodeLayout = this.layout(shrinkedNode.copy());
-        
-        // Is minimal tile size respected?
-        if(this.everyTileIsRespectingMinimalSize(shrinkedNodeLayout)){
-            // ... yes, return layout
-            return shrinkedNodeLayout;
-        } else {
-            // ... no, we must aggregate more tiles
-            numberOfTilesToBeAggregated++;
-        }    
-   
-    } while( numberOfTilesToBeAggregated < node.children.length );
-
-    return shrinkedNodeLayout;
-    
+  return shrinkedNodeLayout;
 };
 
 // Returns true if every tile is respecting minimal size, false otherwise
-LayoutHelper.prototype.everyTileIsRespectingMinimalSize = function(node){
-    
-    var minimalWidth  = this.config.minWidth;
-    var minimalHeight = this.config.minHeight;
-    if(this.isMobilePortrait && this.config.minHeightMobile){
-        minimalHeight = this.config.minHeightMobile;
-    }
-    
-    for(var i=0; i<node.children.length; i++){
-        var tile = node.children[i];
-        var tileWidth  = tile.x1 - tile.x0;
-        var tileHeight = tile.y1 - tile.y0;
-        if( tileWidth < minimalWidth || tileHeight < minimalHeight ){
-            return false;
-        }
-    };
-    return true;
-}
+LayoutHelper.prototype.everyTileIsRespectingMinimalSize = function(node) {
+  var minimalWidth = this.config.minWidth;
+  var minimalHeight = this.config.minHeight;
+  if (this.isMobilePortrait && this.config.minHeightMobile) {
+    minimalHeight = this.config.minHeightMobile;
+  }
 
-// Fuse the n smallest tiles which are smaller than the required size
-LayoutHelper.prototype.aggregateNSmallestTiles = function(node, tilesSortedBySize, numberOfTilesToBeAggregated){
-    
-    if(numberOfTilesToBeAggregated < 2){
-        // Do nothing
-        return node
+  for (var i = 0; i < node.children.length; i++) {
+    var tile = node.children[i];
+    var tileWidth = tile.x1 - tile.x0;
+    var tileHeight = tile.y1 - tile.y0;
+    if (tileWidth < minimalWidth || tileHeight < minimalHeight) {
+      return false;
     }
-        
-    // keep a reference to current obj
-    var layoutHelper = this;
-    
-    // which tiles are to be aggregated? The n smallest
-    var tilesToBeAggregated = tilesSortedBySize.slice(0, numberOfTilesToBeAggregated);
-    
-    // build new tile for aggregate the smallest ones
-    var newTile = {
-        data     : {
-            name     : layoutHelper.config.otherLabel
-        },
-        parent   : node,
-        children : [],
-        value    : 0
-    };
-
-    // compress smallest tiles into one
-    var indexesOfTilesToBeAggregated = [];
-    tilesToBeAggregated.forEach(function(t){
-        node.children.forEach(function(c, i){
-            if(c.data == t.data){
-                newTile.children.push(c);
-                newTile.value += c.value;
-                indexesOfTilesToBeAggregated.push(i);
-            }
-        });
-    });
-    
-    // Customize "other..." tile based on the aggregated ones
-    layoutHelper.config.tileAggregationCallback(newTile)
-    
-    // Keep only non-aggregated tiles
-    node.children = node.children.filter(function(c, i){
-        return indexesOfTilesToBeAggregated.indexOf(i) == -1;
-    });
-    
-    // The "other..." tile is forced to be of average value. The other tiles adapt.
-    var valueAverage = node.value / (node.children.length + 1);
-    var newNodeValue = node.value + valueAverage;
-    newTile.value    = valueAverage;
-    
-    // Add "other..." tile
-    node.children.push(newTile);
-    
-    return node;
+  }
+  return true;
 };
 
-LayoutHelper.prototype.tileArea = function(tile){
-    return (tile.x1 - tile.x0)*(tile.y1 - tile.y0);
-}
+// Fuse the n smallest tiles which are smaller than the required size
+LayoutHelper.prototype.aggregateNSmallestTiles = function(
+  node,
+  tilesSortedBySize,
+  numberOfTilesToBeAggregated
+) {
+  if (numberOfTilesToBeAggregated < 2) {
+    // Do nothing
+    return node;
+  }
+
+  // keep a reference to current obj
+  var layoutHelper = this;
+
+  // which tiles are to be aggregated? The n smallest
+  var tilesToBeAggregated = tilesSortedBySize.slice(
+    0,
+    numberOfTilesToBeAggregated
+  );
+
+  // build new tile for aggregate the smallest ones
+  var newTile = {
+    data: {
+      name: layoutHelper.config.otherLabel
+    },
+    parent: node,
+    children: [],
+    value: 0
+  };
+
+  // compress smallest tiles into one
+  var indexesOfTilesToBeAggregated = [];
+  tilesToBeAggregated.forEach(function(t) {
+    node.children.forEach(function(c, i) {
+      if (c.data == t.data) {
+        newTile.children.push(c);
+        newTile.value += c.value;
+        indexesOfTilesToBeAggregated.push(i);
+      }
+    });
+  });
+
+  // Customize "other..." tile based on the aggregated ones
+  layoutHelper.config.tileAggregationCallback(newTile);
+
+  // Keep only non-aggregated tiles
+  node.children = node.children.filter(function(c, i) {
+    return indexesOfTilesToBeAggregated.indexOf(i) == -1;
+  });
+
+  // The "other..." tile is forced to be of average value. The other tiles adapt.
+  var valueAverage = node.value / (node.children.length + 1);
+  var newNodeValue = node.value + valueAverage;
+  newTile.value = valueAverage;
+
+  // Add "other..." tile
+  node.children.push(newTile);
+
+  return node;
+};
+
+LayoutHelper.prototype.tileArea = function(tile) {
+  return (tile.x1 - tile.x0) * (tile.y1 - tile.y0);
+};
 
 // Make tiles more similar to each other in term of size. Necessary to avoid too much tile aggregation
-LayoutHelper.prototype.flattenTilesValues = function(node){
-    
-    var layoutHelper = this;
-    
-    var valueAverage = node.value / node.children.length;
-    
-    // Flattening process (make big tiles smaller and small tiles bigger)
-    node.value = 0;
-    node.children.forEach(function(c){
-        var flatteningFactor = layoutHelper.config.flatteningFactor;
-        var roundingMean = valueAverage*flatteningFactor;
-        c.value = (c.value + roundingMean)/(flatteningFactor+1);
-        node.value += c.value;
-    });
-    
-    return node;
+LayoutHelper.prototype.flattenTilesValues = function(node) {
+  var layoutHelper = this;
+
+  var valueAverage = node.value / node.children.length;
+
+  // Flattening process (make big tiles smaller and small tiles bigger)
+  node.value = 0;
+  node.children.forEach(function(c) {
+    var flatteningFactor = layoutHelper.config.flatteningFactor;
+    var roundingMean = valueAverage * flatteningFactor;
+    c.value = (c.value + roundingMean) / (flatteningFactor + 1);
+    node.value += c.value;
+  });
+
+  return node;
 };
 
 // Get tiles not respecting the minimal desired size
@@ -193,4 +202,3 @@ LayoutHelper.prototype.flattenTilesValues = function(node){
 }*/
 
 module.exports = LayoutHelper;
-
--- js/Treemap.js
+++ js/Treemap.js
@@ -1,267 +1,272 @@
-
 'use strict';
 
-var TreePath     = require('./TreePath');
+var TreePath = require('./TreePath');
 var LayoutHelper = require('./LayoutHelper');
-var Animator     = require('./Animator');
-var d3           = require('./../node_modules/d3/build/d3.min.js');
-var Handlebars   = require('./../node_modules/handlebars/dist/handlebars.min.js'); 
-var DeviceDetect = require('./vendor/device.min.js')
+var Animator = require('./Animator');
+var d3 = require('./../node_modules/d3/build/d3.min.js');
+var Handlebars = require('./../node_modules/handlebars/dist/handlebars.min.js');
+var DeviceDetect = require('./vendor/device.min.js');
 
-var Treemap = function(config){
-    
-    // Reference to current obj
-    var treemapObj = this;
-    
-    // Save config as class attribute
-    treemapObj.config = config;
+var Treemap = function(config) {
+  // Reference to current obj
+  var treemapObj = this;
 
-    // Load template
-    if(config.tiles.templateSelector){
-        var tplSelector = config.tiles.templateSelector.replace('#', '');
-        var tileTplSource = document.getElementById(tplSelector).innerHTML;
-        treemapObj.tileTpl = Handlebars.compile(tileTplSource);
-    }
-    
-    treemapObj.loadDataAndInit();
+  // Save config as class attribute
+  treemapObj.config = config;
+
+  // Load template
+  if (config.tiles.templateSelector) {
+    var tplSelector = config.tiles.templateSelector.replace('#', '');
+    var tileTplSource = document.getElementById(tplSelector).innerHTML;
+    treemapObj.tileTpl = Handlebars.compile(tileTplSource);
+  }
+
+  treemapObj.loadDataAndInit();
 };
 
-Treemap.prototype.loadDataAndInit = function(){
-    
-    var treemapObj = this;
-    
-    // Load data and then draw the treemap
-    d3.json(treemapObj.config.datasetURL, function(error, json){
-        if (error) {
-            console.error(error);
-        } else {
-            
-            // prepare data
-            treemapObj.data = d3.hierarchy(json);
-            treemapObj.data.sum(function(d){
-                    if(d.children){
-                        return 0;
-                    } else {
-                        return 1;
-                    }
-                })
-                .each(function(d){
-                    
-                    // store a copy of value because we will mess it up
-                    d.data.originalValue = d.value;
-                    
-                    // launch custom callback
-                    if(treemapObj.config.dataPreparationCallback){
-                        treemapObj.config.dataPreparationCallback(d);
-                    }
-                })
-                .sort(function(){
-                    if(treemapObj.config.sortCallback){
-                        return treemapObj.config.sortCallback;
-                    }
-                });
-                
-            // init and launch treemap
-            treemapObj.initTreemap();
-            treemapObj.updateTreemap(treemapObj.data);
-        }
+Treemap.prototype.loadDataAndInit = function() {
+  var treemapObj = this;
+
+  // Load data and then draw the treemap
+  d3.json(treemapObj.config.datasetURL, function(error, json) {
+    if (error) {
+      console.error(error);
+    } else {
+      // prepare data
+      treemapObj.data = d3.hierarchy(json);
+      treemapObj.data
+        .sum(function(d) {
+          if (null === d.info) return 0;
+          return Math.abs(d.info.score * 1000);
+        })
+        .each(function(d) {
+          // store a copy of value because we will mess it up
+          d.data.originalValue = d.value;
+
+          // launch custom callback
+          if (treemapObj.config.dataPreparationCallback) {
+            treemapObj.config.dataPreparationCallback(d);
+          }
+        })
+        .sort(function() {
+          if (treemapObj.config.sortCallback) {
+            return treemapObj.config.sortCallback;
+          }
+        });
+
+      // init and launch treemap
+      treemapObj.initTreemap();
+      treemapObj.updateTreemap(treemapObj.data);
+    }
+  });
+};
+
+Treemap.prototype.initTreemap = function() {
+  // Reference to current obj
+  var treemapObj = this;
+
+  // Mobile portrait layout?
+  treemapObj.isMobilePortrait =
+    DeviceDetect.mobile() && DeviceDetect.portrait();
+
+  // Use TreePath object to keep track of navigation
+  treemapObj.treePath = new TreePath(treemapObj.data);
+
+  // Use Animator object to manage animations
+  treemapObj.animator = new Animator(treemapObj.config);
+
+  // Create main <div> and resize it
+  treemapObj.selection = d3
+    .select(treemapObj.config.containerSelector)
+    .style('width', treemapObj.config.width)
+    .style('height', treemapObj.config.height);
+
+  // Save width and height in pixels
+  treemapObj.config.width = parseInt(treemapObj.selection.style('width'), 10);
+  treemapObj.config.height = parseInt(treemapObj.selection.style('height'), 10);
+
+  // Add breadcumbs / navigation bar
+  treemapObj.navigationHeight = parseInt(
+    treemapObj.config.breadCumbs.height.replace('px', ''),
+    10
+  );
+  treemapObj.navigation = treemapObj.selection
+    .append('div')
+    .attr('id', 'treemap-navigation')
+    .style('width', '100%')
+    .style('height', treemapObj.navigationHeight + 'px');
+
+  // Add breadcumbs text and buttons
+  treemapObj.breadCumbs = treemapObj.navigation
+    .append('span')
+    .attr('id', 'treemap-navigation-breadcumbs')
+    .style('cursor', 'pointer');
+  treemapObj.updateBreadCrumbs();
+
+  treemapObj.navigation
+    .append('span')
+    .attr('id', 'treemap-navigation-up')
+    .text(' UP')
+    .style('cursor', 'pointer')
+    .on('click', function() {
+      treemapObj.upOneLevel();
     });
-}
+  treemapObj.navigation
+    .append('span')
+    .text('home')
+    .attr('id', 'treemap-navigation-home')
+    .text(' HOME')
+    .style('cursor', 'pointer')
+    .on('click', function() {
+      treemapObj.upToRoot();
+    });
 
-Treemap.prototype.initTreemap = function(){
-    
-    // Reference to current obj
-    var treemapObj = this;
-    
-    // Mobile portrait layout?
-    treemapObj.isMobilePortrait = DeviceDetect.mobile() && DeviceDetect.portrait();
-    
-    // Use TreePath object to keep track of navigation
-    treemapObj.treePath = new TreePath(treemapObj.data);
-    
-    // Use Animator object to manage animations
-    treemapObj.animator = new Animator(treemapObj.config);
-        
-    // Create main <div> and resize it
-    treemapObj.selection = d3.select( treemapObj.config.containerSelector )
-        .style('width', treemapObj.config.width)
-        .style('height', treemapObj.config.height)
+  // Add tiles container
+  treemapObj.tilesContainerHeight =
+    treemapObj.config.height - treemapObj.navigationHeight;
+  treemapObj.selection
+    .append('div')
+    .attr('id', 'treemap-tiles-container')
+    .style('width', '100%')
+    .style('height', treemapObj.tilesContainerHeight + 'px')
+    .style('position', 'relative');
 
-    // Save width and height in pixels
-    treemapObj.config.width = parseInt( treemapObj.selection.style('width'), 10 );
-    treemapObj.config.height = parseInt( treemapObj.selection.style('height'), 10 );
-    
-    // Add breadcumbs / navigation bar
-    treemapObj.navigationHeight = parseInt( treemapObj.config.breadCumbs.height.replace('px', ''), 10 );
-    treemapObj.navigation = treemapObj.selection.append('div')
-        .attr('id', 'treemap-navigation')
-        .style('width', '100%')
-        .style('height', treemapObj.navigationHeight + 'px');
-
-    // Add breadcumbs text and buttons
-    treemapObj.breadCumbs = treemapObj.navigation.append('span')
-        .attr('id', 'treemap-navigation-breadcumbs')
-        .style('cursor', 'pointer');
-    treemapObj.updateBreadCrumbs();
-        
-    treemapObj.navigation.append('span')
-        .attr('id', 'treemap-navigation-up')
-        .text(' UP')
-        .style('cursor', 'pointer')
-        .on('click', function(){
-            treemapObj.upOneLevel();
-        });
-    treemapObj.navigation.append('span').text('home')
-        .attr('id', 'treemap-navigation-home') 
-        .text(' HOME')
-        .style('cursor', 'pointer')
-        .on('click', function(){
-            treemapObj.upToRoot();
-        });
-
-    // Add tiles container
-    treemapObj.tilesContainerHeight = treemapObj.config.height - treemapObj.navigationHeight;
-    treemapObj.selection.append('div')
-        .attr('id', 'treemap-tiles-container')
-        .style('width', '100%')
-        .style('height', treemapObj.tilesContainerHeight + 'px')
-        .style('position', 'relative');
-
-
-    // Use Layout helper to manage layout
-    var layoutSize = [treemapObj.config.width, treemapObj.tilesContainerHeight];
-    treemapObj.layoutHelper = new LayoutHelper(layoutSize, treemapObj.isMobilePortrait, treemapObj.config);
-    
+  // Use Layout helper to manage layout
+  var layoutSize = [treemapObj.config.width, treemapObj.tilesContainerHeight];
+  treemapObj.layoutHelper = new LayoutHelper(
+    layoutSize,
+    treemapObj.isMobilePortrait,
+    treemapObj.config
+  );
 };
 
-Treemap.prototype.updateTreemap = function(){
-    
-    // Reference to current obj
-    var treemapObj = this;
-    
-    // Update breadCumbs
-    treemapObj.updateBreadCrumbs();
+Treemap.prototype.updateTreemap = function() {
+  // Reference to current obj
+  var treemapObj = this;
 
-    // run layout for current node
-    var currentNode = treemapObj.treePath.currentNode();
-    var layoutNode = treemapObj.layoutHelper.getAdjustedLayout(currentNode);  // adjust tile size
+  // Update breadCumbs
+  treemapObj.updateBreadCrumbs();
 
-    treemapObj.selection.selectAll('.tile').remove();
-    var tiles = treemapObj.selection.select('#treemap-tiles-container').selectAll('.tile')
-        .data(layoutNode.children)
-        .enter()
-        .append('div')
-        .attr('class', 'tile')
-        .style('position', 'absolute')
-        .style('top', function(d){
-            return d.y0 + 'px';
-        })
-        .style('left', function(d){
-            return d.x0 + 'px';
-        })
-        .style('width', function(d){
-            return (d.x1 - d.x0) + 'px';
-        })
-        .style('height', function(d){
-            return (d.y1 - d.y0) + 'px';
-        })
-        .style('background-color', function(d, i){
-            return d3.schemeCategory20[i%20];
-        })
-        .style('cursor', 'pointer')
-        .html(function(d){
-            if(treemapObj.tileTpl){
-                return treemapObj.tileTpl(d);
-            } else {
-                return d.data.name;
-            }
-        })
-        .style('opacity', 0.0)
-        .on('click', function(d){
-            if(d.children){
-                
-                // Run expand animation and then update the treemap
-                var element = this;
-                treemapObj.animator.expandNode(treemapObj.selection, element, function(){
-                    // This will be executed at after the animation finished
-                    treemapObj.downTo(d);
-                });
+  // run layout for current node
+  var currentNode = treemapObj.treePath.currentNode();
+  var layoutNode = treemapObj.layoutHelper.getAdjustedLayout(currentNode); // adjust tile size
 
-            } else {
-                treemapObj.leafClicked(d);
-            }
-        });
-    
-    tiles.transition()
-        .style('opacity', 1.0)
-        .duration(treemapObj.config.animationDuration);
+  treemapObj.selection.selectAll('.tile').remove();
+  var tiles = treemapObj.selection
+    .select('#treemap-tiles-container')
+    .selectAll('.tile')
+    .data(layoutNode.children)
+    .enter()
+    .append('div')
+    .attr('class', 'tile')
+    .style('position', 'absolute')
+    .style('top', function(d) {
+      return d.y0 + 'px';
+    })
+    .style('left', function(d) {
+      return d.x0 + 'px';
+    })
+    .style('width', function(d) {
+      return d.x1 - d.x0 + 'px';
+    })
+    .style('height', function(d) {
+      return d.y1 - d.y0 + 'px';
+    })
+    .style('background-color', function(d, i) {
+      return d3.schemeCategory20[i % 20];
+    })
+    .style('cursor', 'pointer')
+    .html(function(d) {
+      if (treemapObj.tileTpl) {
+        return treemapObj.tileTpl(d);
+      } else {
+        return d.data.name;
+      }
+    })
+    .style('opacity', 0.0)
+    .on('click', function(d) {
+      if (d.children) {
+        // Run expand animation and then update the treemap
+        var element = this;
+        treemapObj.animator.expandNode(
+          treemapObj.selection,
+          element,
+          function() {
+            // This will be executed at after the animation finished
+            treemapObj.downTo(d);
+          }
+        );
+      } else {
+        treemapObj.leafClicked(d);
+      }
+    });
+
+  tiles
+    .transition()
+    .style('opacity', 1.0)
+    .duration(treemapObj.config.animationDuration);
 };
 
-Treemap.prototype.updateBreadCrumbs = function(){
-    
-    var treemapObj = this;
-    var separator  = this.config.breadCumbs.separator;
-    var ancestors  = this.treePath.path;
-    
-    this.breadCumbs.selectAll('.breadCrumbLevel').remove();
-    this.breadCumbs.selectAll('.breadCrumbLevel')
-        .data(ancestors)
-        .enter()
-        .append('span')
-        .attr('class', 'breadCrumbLevel')
-        .on('click', function(d, i){
-            var stepsBack = ancestors.length - i - 1;
-            for(var step=0; step<stepsBack; step++){
-                treemapObj.upOneLevel();
-            }
-        })
-        .text(function(d){
-            return d.data.name+ ' ' + separator;
-        });
+Treemap.prototype.updateBreadCrumbs = function() {
+  var treemapObj = this;
+  var separator = this.config.breadCumbs.separator;
+  var ancestors = this.treePath.path;
+
+  this.breadCumbs.selectAll('.breadCrumbLevel').remove();
+  this.breadCumbs
+    .selectAll('.breadCrumbLevel')
+    .data(ancestors)
+    .enter()
+    .append('span')
+    .attr('class', 'breadCrumbLevel')
+    .on('click', function(d, i) {
+      var stepsBack = ancestors.length - i - 1;
+      for (var step = 0; step < stepsBack; step++) {
+        treemapObj.upOneLevel();
+      }
+    })
+    .text(function(d) {
+      return d.data.name + ' ' + separator;
+    });
 };
 
-Treemap.prototype.downTo = function(node){
-    
-    // update path
-    this.treePath.downTo(node);
-    
-    // jump level if there is only one children
-    if(node.children && (node.children.length == 1) ){
-        var nextNode = node.children[0];
-        if(nextNode.children){
-            this.downTo(nextNode);
-        }
+Treemap.prototype.downTo = function(node) {
+  // update path
+  this.treePath.downTo(node);
+
+  // jump level if there is only one children
+  if (node.children && node.children.length == 1) {
+    var nextNode = node.children[0];
+    if (nextNode.children) {
+      this.downTo(nextNode);
     }
-    
-    // display subNode
-    this.updateTreemap();
+  }
+
+  // display subNode
+  this.updateTreemap();
 };
 
-Treemap.prototype.upOneLevel = function(){
-    
-    // update path
-    this.treePath.upOneLevel();
-    
-    // display parent node
-    this.updateTreemap();
+Treemap.prototype.upOneLevel = function() {
+  // update path
+  this.treePath.upOneLevel();
+
+  // display parent node
+  this.updateTreemap();
 };
 
-Treemap.prototype.upToRoot = function(){
-    
-    // update path
-    this.treePath.upToRoot();
-    
-    // display root node
-    this.updateTreemap();
+Treemap.prototype.upToRoot = function() {
+  // update path
+  this.treePath.upToRoot();
+
+  // display root node
+  this.updateTreemap();
 };
 
-Treemap.prototype.leafClicked = function(d){
-    if(this.config.tileClickedCallback){
-        this.config.tileClickedCallback(d);
-    }
+Treemap.prototype.leafClicked = function(d) {
+  if (this.config.tileClickedCallback) {
+    this.config.tileClickedCallback(d);
+  }
 };
 
-window.Treemap = Treemap; 
+window.Treemap = Treemap;
 module.exports = Treemap;
-
-
